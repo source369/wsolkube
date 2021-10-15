89f62fd415b81a96cb316295b3aade3a6015ada0
neo <neo89ucsc@gmail.com>
2021-10-16 02:31:24 +0530

    dev up

--- app/functions/live_pods.py
+++ app/functions/live_pods.py
@@ -1,20 +1,11 @@
 
-import base64
-import json
 
 consul_key_value = {"Key": "Value"}
 
 # function
-def live_pods():
-    with open('../../data.json') as file:
-        data = json.load(file)
-        for x in range(len(data)):
-            base64_message = data[x]["Value"]
-            base64_bytes = base64_message.encode('ascii')
-            message_bytes = base64.b64decode(base64_bytes)
-            message = message_bytes.decode('ascii')
-            consul_key_value[(data[x]["Key"]).rsplit('/', 1)[1]] = message
-    # print(consul_key_value)
+def _pods(env):
+    click.echo('Environment is %s!' % env)
+    print(env)
 
 
 
--- wsol.py
+++ wsol.py
@@ -1,6 +1,6 @@
 import click
+from app.functions.live_pods import *
 from wheel import cli
-
 
 @click.group()
 def main():
@@ -21,9 +21,35 @@ def main():
 #     for x in range(count):
 #         click.echo('Hello %s!' % name)
 
+# @click.command()
+
+# q_env = input ("Env :")
+# q_key = input ("Key :")
+# q_pwd = getpass.getpass(prompt='Password: ', stream=None)
+#
+def pods(env):
+    print('Environment is %s!' % env)
+
+def _app_nav(q_env,keyring,pwd):
+    if q_env.lower() == 'dev':
+        env = 'kubectl --kubeconfig $(pwd)/kubeconfig -n writing-solutions-dev'
+    elif q_env.lower() == 'qa':
+        env = 'kubectl --kubeconfig $(pwd)/kubeconfig -n writing-solutions-qa'
+    elif q_env.lower() == 'int':
+        env = 'kubectl --kubeconfig $(pwd)/kubeconfig -n writing-solutions-int'
+    elif q_env.lower() == 'nft':
+        env = 'kubectl --kubeconfig $(pwd)/kubeconfig -n writing-solutions-nft'
+    elif q_env.lower() == 'stg':
+        env = 'kubectl --kubeconfig $(pwd)/kubeconfig -n writing-solutions-stg'
+
+    _pods(env)
+
 @click.command()
 @click.option('-c','--count', default=1, help='Number of greetings.')
-@click.option('-n','--name', prompt='Your name', help='The person to greet.')
-def init(count, name):
-    for x in range(count):
-        click.echo('Hello %s!' % name)
+@click.option('-e','--env', prompt='Environment', help='What Env : DEV/QE/INT/NFT/STG/PROD')
+@click.option('-k','--keyring', prompt='Keyring', help='Enter KeyRing Value')
+@click.option('-p','--pwd', prompt='Password', help='Enter your Password ')
+def init(count, env, keyring,pwd):
+    _app_nav(env,keyring,pwd)
+
+
